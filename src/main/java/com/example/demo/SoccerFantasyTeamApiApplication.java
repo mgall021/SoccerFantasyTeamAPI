ba4c056d7d15e93cddfd77ae05efa835fad04bd4
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-18 23:00:50 -0700

    test commit

--- src/main/java/com/example/demo/SoccerFantasyTeamApiApplication.java
+++ src/main/java/com/example/demo/SoccerFantasyTeamApiApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SoccerFantasyTeamApiApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SoccerFantasyTeamApiApplication.class, args);
+	}{
+		System.out.println("HelloWorld");
 	}
 
 }
