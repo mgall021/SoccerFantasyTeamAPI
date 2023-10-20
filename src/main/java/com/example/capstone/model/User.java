5a4a81194b837ce21dd82768e773a0f822f52b39
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-19 23:15:48 -0700

    annotate User Model and display table name

--- src/main/java/com/example/capstone/model/User.java
+++ src/main/java/com/example/capstone/model/User.java
@@ -1,5 +1,10 @@
 package com.example.capstone.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
     private Long id;
 
