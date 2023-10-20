d4482a45fe966aaed74bc5609436a73c504a21d7
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-20 12:47:58 -0700

    create MyUserDETAILSSERVICE CLASS

---
+++ src/main/java/com/example/capstone/security/MyUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.example.capstone.security;
+
+com.example.musicstreamingapi.model.User;
+import com.example.musicstreamingapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String emailAddress) throws UsernameNotFoundException {
+        User user = userService.findUserByEmailAddress(emailAddress);
+        return new MyUserDetails(user);
+    }
+}
