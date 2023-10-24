2353211e56abeb5c440f321a7ab852c04f396644
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-24 01:20:08 -0700

    instantiate soccerPlayere Repo and List of Soccer players inside service

--- src/main/java/com/example/capstone/service/SoccerPlayerService.java
+++ src/main/java/com/example/capstone/service/SoccerPlayerService.java
@@ -1,7 +1,20 @@
 package com.example.capstone.service;
 
+import com.example.capstone.model.SoccerPlayer;
+import com.example.capstone.repository.SoccerPlayerRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SoccerPlayerService {
+
+    private SoccerPlayerRepository soccerPlayerRepository;
+
+    private final List<SoccerPlayer> soccerPlayersList;
+
+    public SoccerPlayerService(SoccerPlayerRepository soccerPlayerRepository, List<SoccerPlayer> soccerPlayersList) {
+        this.soccerPlayerRepository = soccerPlayerRepository;
+        this.soccerPlayersList = soccerPlayersList;
+    }
 }
