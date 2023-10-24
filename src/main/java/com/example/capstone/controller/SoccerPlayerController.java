ea88ce78c54d50cb7d8ca8f881f7b341956481a8
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-24 01:31:57 -0700

    annotate SoccerPlayerController

--- src/main/java/com/example/capstone/controller/SoccerPlayerController.java
+++ src/main/java/com/example/capstone/controller/SoccerPlayerController.java
@@ -1,4 +1,10 @@
 package com.example.capstone.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/soccerplayers")
 public class SoccerPlayerController {
+
 }
