10d97ac38b194b69941156b914d2e16de959170e
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-23 19:45:51 -0700

    create user step Passed

--- src/main/java/com/example/capstone/controller/UserController.java
+++ src/main/java/com/example/capstone/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.example.capstone.controller;
 
+import com.example.capstone.model.User;
 import com.example.capstone.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +20,13 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/register/")
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        User createdUser = userService.createUser(user);
+        response.put("message", "success");
+        response.put("data", createdUser);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 }
