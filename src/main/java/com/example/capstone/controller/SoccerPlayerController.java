25f8c062e4666c03ffa2e154ef35fe44bf3d22c5
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-24 01:33:31 -0700

    autowire soccerplayer service

--- src/main/java/com/example/capstone/controller/SoccerPlayerController.java
+++ src/main/java/com/example/capstone/controller/SoccerPlayerController.java
@@ -1,10 +1,17 @@
 package com.example.capstone.controller;
 
+import com.example.capstone.service.SoccerPlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/soccerplayers")
 public class SoccerPlayerController {
+    private final SoccerPlayerService soccerPlayerService;
 
+    @Autowired
+    public SoccerPlayerController(SoccerPlayerService soccerPlayerService) {
+        this.soccerPlayerService = soccerPlayerService;
+    }
 }
