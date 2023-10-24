5dae0c8657eecf420ed82f34205babf8cd6c12f0
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-23 19:28:44 -0700

    write a getJwt method

--- src/test/java/definitions/UserControllerTestDefs.java
+++ src/test/java/definitions/UserControllerTestDefs.java
@@ -8,6 +8,8 @@ import io.cucumber.spring.CucumberContextConfiguration;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -33,6 +35,27 @@ public class UserControllerTestDefs {
     private static RequestSpecification request = RestAssured.given();
 
 
+    public String getJWTKey() throws JSONException {
+        // Set the base URI and create a request
+        RestAssured.baseURI = BASE_URL;
+        RequestSpecification request = RestAssured.given();
+
+        // Set the content-type header to indicate JSON data
+        request.header("Content-Type", "application/json");
+
+        String endpoint = BASE_URL + port + "/auth/users/login/";
+
+        // Create a JSON request body with user email and password
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("emailAddress", "suresh@g.com");
+        requestBody.put("password", "suresh123");
+
+        // Send a POST request to the authentication endpoint
+        Response response = request.body(requestBody.toString()).post(endpoint);
+
+        // Extract and return the JWT key from the authentication response
+        return response.jsonPath().getString("jwt");
+    }
 
     @When("I create an account")
     public void iCreateAnAccount() {
