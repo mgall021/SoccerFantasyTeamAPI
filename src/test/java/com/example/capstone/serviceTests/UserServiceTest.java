74c57be3de48fd416200821c7b835c93a05be890
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-23 17:30:55 -0700

    create void method to initalize mockito annotations

--- src/test/java/com/example/capstone/serviceTests/UserServiceTest.java
+++ src/test/java/com/example/capstone/serviceTests/UserServiceTest.java
@@ -2,12 +2,20 @@ package com.example.capstone.serviceTests;
 
 import com.example.capstone.repository.UserRepository;
 import com.example.capstone.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 public class UserServiceTest {
     @InjectMocks
     private UserService userService;
     @Mock
     private UserRepository userRepository;
+
+    @BeforeEach // Initializes Mockito annotations before each test method.
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
+
 }
