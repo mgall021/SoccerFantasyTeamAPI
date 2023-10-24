86734e42a8ba7ca96b36a0053d5d80d477725a4a
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-23 19:44:06 -0700

    instantiate UserService inside UserController

--- src/main/java/com/example/capstone/controller/UserController.java
+++ src/main/java/com/example/capstone/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.capstone.controller;
 
 import com.example.capstone.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,10 @@ import java.util.HashMap;
 @RestController
 @RequestMapping("/auth/users")
 public class UserController {
-
-
+    private final UserService userService;
+    HashMap<String, Object > response = new HashMap<>();
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 }
