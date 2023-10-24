817001f3c16e6b6ef162e4e1e21dae104cc4cce8
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-23 19:21:44 -0700

    create Cucumber test for creating User

--- src/test/java/com/example/capstone/definitions/UserControllerTestDefs.java
+++ src/test/java/com/example/capstone/definitions/UserControllerTestDefs.java
@@ -1,9 +1,77 @@
 package com.example.capstone.definitions;
 
+import com.example.capstone.SoccerFantasyTeamApiApplication;
+import com.example.capstone.service.UserService;
 import io.cucumber.java.en.When;
+import io.cucumber.spring.CucumberContextConfiguration;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
+import java.util.logging.Logger;
+
+@CucumberContextConfiguration()
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = SoccerFantasyTeamApiApplication.class)
 public class UserControllerTestDefs {
-    @When("I create an account")
-    public void iCreateAnAccount() {
+    //    private static final Logger logger = Logger.getLogger(FantasyTeamTestDefs.class.getName());
+    private static Response response;
+    private String message;
+
+    @LocalServerPort
+    String port;
+
+    @Autowired
+    private UserService userService;
+
+    private final static String BASE_URL = "http://localhost:";
+
+    private static RequestSpecification request = RestAssured.given();
+
+    public String getJWTKey() throws JSONException {
+        // Set the base URI and create a request
+        RestAssured.baseURI = BASE_URL;
+        RequestSpecification request = RestAssured.given();
+
+        // Set the content-type header to indicate JSON data
+        request.header("Content-Type", "application/json");
+
+        String endpoint = BASE_URL + port + "/auth/users/login/";
+
+        // Create a JSON request body with user email and password
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("emailAddress", "suresh@g.com");
+        requestBody.put("password", "suresh123");
+
+        // Send a POST request to the authentication endpoint
+        Response response = request.body(requestBody.toString()).post(endpoint);
+
+        // Extract and return the JWT key from the authentication response
+        return response.jsonPath().getString("jwt");
     }
+
+
+    @When("I create an account")
+    public void iCreateAnAccount() throws JSONException {
+        String endpoint = BASE_URL + port + "/auth/users/register/";
+        JSONObject requestBody = new JSONObject()
+                .put("firstName", "Suresh")
+                .put("lastName", "Sigera")
+                .put("emailAddress", "suresh@g.com")
+                .put("password", "suresh123");
+
+        response = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(requestBody.toString())
+                .post(endpoint);
+
+        Assert.assertEquals(201, response.getStatusCode());
+    }
+
 }
---
+++ src/test/java/com/example/capstone/runners/TestRunner.java
@@ -0,0 +1,4 @@
+package com.example.capstone.runners;
+
+public class TestRunner {
+}
