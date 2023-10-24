41cc3d129d2c83766d3f97a1a94b6448f60d3184
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-23 20:10:37 -0700

    write service method to update user, test passed

--- src/main/java/com/example/capstone/service/UserService.java
+++ src/main/java/com/example/capstone/service/UserService.java
@@ -67,6 +67,23 @@ public class UserService {
         }
     }
 
+    public User updateUser(Long userId, User updatedUser) {
+        // Check if the user with the given ID exists
+        Optional<User> existingUserOptional = userRepository.findById(userId);
+        if (existingUserOptional.isPresent()) {
+            User existingUser = existingUserOptional.get();
+            // Update the fields you want to change
+            existingUser.setName(updatedUser.getName());
+            // Save the updated user
+            return userRepository.save(existingUser);
+        } else {
+            // Handle the case where the user with the given ID doesn't exist
+            throw new IllegalArgumentException();
+        }
+    }
+
+
+
 
 
 
