724a69a024eca9f26648ab4b0e2be44076bb8102
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-23 19:43:22 -0700

    annotate UserController class

--- src/main/java/com/example/capstone/controller/UserController.java
+++ src/main/java/com/example/capstone/controller/UserController.java
@@ -1,4 +1,14 @@
 package com.example.capstone.controller;
 
+import com.example.capstone.service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/auth/users")
 public class UserController {
+
+
 }
