5c77daff79bf8e3ddff49ba5759ad7026fb5eb2d
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-24 19:56:06 -0700

    create logger for fabtasyTeam service

--- src/main/java/com/example/capstone/service/FantasyTeamService.java
+++ src/main/java/com/example/capstone/service/FantasyTeamService.java
@@ -5,6 +5,8 @@ import com.example.capstone.repository.SoccerPlayerRepository;
 import com.example.capstone.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.logging.Logger;
+
 @Service
 public class FantasyTeamService {
     private final FantasyTeamRepository fantasyTeamRepository;
@@ -12,6 +14,8 @@ public class FantasyTeamService {
     private final UserRepository userRepository;
 
     private final SoccerPlayerRepository soccerPlayerRepository;
+
+    private final Logger logger = Logger.getLogger(FantasyTeamService.class.getName());
 
     public FantasyTeamService(FantasyTeamRepository fantasyTeamRepository, UserRepository userRepository, SoccerPlayerRepository soccerPlayerRepository) {
         this.fantasyTeamRepository = fantasyTeamRepository;
