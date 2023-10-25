4a1c1824b7153b97fe5dfcecbb5baed326acbe93
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-24 20:11:08 -0700

    create post method for fantasyteam controller, test passes

--- src/main/java/com/example/capstone/controller/FantasyTeamController.java
+++ src/main/java/com/example/capstone/controller/FantasyTeamController.java
@@ -1,9 +1,12 @@
 package com.example.capstone.controller;
 
+import com.example.capstone.model.FantasyTeam;
 import com.example.capstone.service.FantasyTeamService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/fantasyTeam")
@@ -11,4 +14,8 @@ public class FantasyTeamController {
     @Autowired
     private FantasyTeamService fantasyTeamService;
 
+    @PostMapping
+    public ResponseEntity<FantasyTeam> createFantasyTeam(@RequestBody FantasyTeam fantasyTeam) {
+        return ResponseEntity.ok(fantasyTeamService.createFantasyTeam(fantasyTeam));
+    }
 }
