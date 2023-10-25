e3ffead8dbc364b32d573d822578cf1b162d0dba
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-24 20:06:51 -0700

    write service method to create a team with at most 11 players

--- src/main/java/com/example/capstone/service/FantasyTeamService.java
+++ src/main/java/com/example/capstone/service/FantasyTeamService.java
@@ -1,5 +1,6 @@
 package com.example.capstone.service;
 
+import com.example.capstone.model.FantasyTeam;
 import com.example.capstone.repository.FantasyTeamRepository;
 import com.example.capstone.repository.SoccerPlayerRepository;
 import com.example.capstone.repository.UserRepository;
@@ -18,5 +19,12 @@ public class FantasyTeamService {
 
     private final Logger logger = Logger.getLogger(FantasyTeamService.class.getName());
 
+
+    public FantasyTeam createFantasyTeam(FantasyTeam fantasyTeam) {
+        if (fantasyTeam.getSoccerPlayers().size() > 11) {
+            throw new IllegalArgumentException("A fantasy team can have at most 11 players.");
+        }
+        return fantasyTeamRepository.save(fantasyTeam);
+    }
 
 }
