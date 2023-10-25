af961ec784eb332bcd422e299e2f205aed102841
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-24 21:02:16 -0700

    create test to add player to a team

--- src/test/java/com/example/capstone/serviceTests/FantasyTeamServiceTest.java
+++ src/test/java/com/example/capstone/serviceTests/FantasyTeamServiceTest.java
@@ -2,6 +2,7 @@ package com.example.capstone.serviceTests;
 
 
 import com.example.capstone.model.FantasyTeam;
+import com.example.capstone.model.SoccerPlayer;
 import com.example.capstone.repository.FantasyTeamRepository;
 import com.example.capstone.repository.SoccerPlayerRepository;
 import com.example.capstone.service.FantasyTeamService;
@@ -38,6 +39,20 @@ public class FantasyTeamServiceTest {
 
         FantasyTeam result = fantasyTeamService.createFantasyTeam(team);
         assertEquals(result, team);
+    }
+    @Test
+    public void testAddPlayerToTeam() {
+        FantasyTeam team = new FantasyTeam();
+        team.setSoccerPlayers(new HashSet<>());
+
+        SoccerPlayer player = new SoccerPlayer();
+
+        when(fantasyTeamRepository.findById(1L)).thenReturn(Optional.of(team));
+        when(soccerPlayerRepository.findById(1L)).thenReturn(Optional.of(player));
+        when(fantasyTeamRepository.save(any(FantasyTeam.class))).thenReturn(team);
+
+        FantasyTeam result = fantasyTeamService.addPlayerToTeam(1L, 1L);
+        assertTrue(result.getSoccerPlayers().contains(player));
     }
 
 
