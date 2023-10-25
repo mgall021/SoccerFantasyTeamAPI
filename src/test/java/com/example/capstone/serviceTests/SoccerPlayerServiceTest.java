6b083a3d50fdcfe1cc11c3139b3d882bd7c89e11
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-24 18:07:18 -0700

    instantiate service and controller classes inside test

--- src/test/java/com/example/capstone/serviceTests/SoccerPlayerServiceTest.java
+++ src/test/java/com/example/capstone/serviceTests/SoccerPlayerServiceTest.java
@@ -1,11 +1,17 @@
 package com.example.capstone.serviceTests;
 
+import com.example.capstone.controller.SoccerPlayerController;
 import com.example.capstone.service.SoccerPlayerService;
+import com.nimbusds.jose.crypto.impl.AAD;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class SoccerPlayerServiceTest {
+    @Autowired
     private SoccerPlayerService soccerPlayerService;
+    @Autowired
+    private SoccerPlayerController soccerPlayerController;
 
 
 }
