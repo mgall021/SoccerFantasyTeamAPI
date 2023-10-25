b6f1f4d5511a3acf19dc7297b8ac083717371278
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-24 19:51:38 -0700

    create a beforeeach mock to make sure mocks run as they should

--- src/test/java/com/example/capstone/serviceTests/FantasyTeamServiceTest.java
+++ src/test/java/com/example/capstone/serviceTests/FantasyTeamServiceTest.java
@@ -4,8 +4,10 @@ package com.example.capstone.serviceTests;
 import com.example.capstone.repository.FantasyTeamRepository;
 import com.example.capstone.repository.SoccerPlayerRepository;
 import com.example.capstone.service.FantasyTeamService;
+import org.junit.jupiter.api.BeforeEach;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 public class FantasyTeamServiceTest {
     @InjectMocks
@@ -16,5 +18,10 @@ public class FantasyTeamServiceTest {
 
     @Mock
     private SoccerPlayerRepository soccerPlayerRepository;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
 
 }
