168f93fa159c94709fb9ab5b2492ba56714a29db
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-24 20:09:13 -0700

    create link from fabtasy team controller to its service class

--- src/main/java/com/example/capstone/controller/FantasyTeamController.java
+++ src/main/java/com/example/capstone/controller/FantasyTeamController.java
@@ -1,10 +1,14 @@
 package com.example.capstone.controller;
 
+import com.example.capstone.service.FantasyTeamService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/fantasyTeam")
 public class FantasyTeamController {
+    @Autowired
+    private FantasyTeamService fantasyTeamService;
 
 }
