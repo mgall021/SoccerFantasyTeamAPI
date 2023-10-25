a7d0c699c6c5f0ccd68f0bfad49e1cecd95bdf16
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-24 23:25:07 -0700

    create constructor and autowire it

--- src/main/java/com/example/capstone/seed/SeedData.java
+++ src/main/java/com/example/capstone/seed/SeedData.java
@@ -3,6 +3,8 @@ package com.example.capstone.seed;
 import com.example.capstone.repository.FantasyTeamRepository;
 import com.example.capstone.repository.SoccerPlayerRepository;
 import com.example.capstone.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -16,5 +18,11 @@ public class SeedData {
 
     private final PasswordEncoder passwordEncoder;
 
-
+    @Autowired
+    public SeedData(FantasyTeamRepository fantasyTeamRepository, SoccerPlayerRepository soccerPlayerRepository, UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder) {
+        this.fantasyTeamRepository = fantasyTeamRepository;
+        this.soccerPlayerRepository = soccerPlayerRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 }
