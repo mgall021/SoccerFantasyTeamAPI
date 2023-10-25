d15437e0e11911875d3a4f5e6fee848699342d99
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-24 20:03:28 -0700

    current service for fabtasyteam

--- src/main/java/com/example/capstone/service/FantasyTeamService.java
+++ src/main/java/com/example/capstone/service/FantasyTeamService.java
@@ -3,23 +3,20 @@ package com.example.capstone.service;
 import com.example.capstone.repository.FantasyTeamRepository;
 import com.example.capstone.repository.SoccerPlayerRepository;
 import com.example.capstone.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.logging.Logger;
 
 @Service
 public class FantasyTeamService {
-    private final FantasyTeamRepository fantasyTeamRepository;
+    @Autowired
+    private FantasyTeamRepository fantasyTeamRepository;
 
-    private final UserRepository userRepository;
-
-    private final SoccerPlayerRepository soccerPlayerRepository;
+    @Autowired
+    private  SoccerPlayerRepository soccerPlayerRepository;
 
     private final Logger logger = Logger.getLogger(FantasyTeamService.class.getName());
 
-    public FantasyTeamService(FantasyTeamRepository fantasyTeamRepository, UserRepository userRepository, SoccerPlayerRepository soccerPlayerRepository) {
-        this.fantasyTeamRepository = fantasyTeamRepository;
-        this.userRepository = userRepository;
-        this.soccerPlayerRepository = soccerPlayerRepository;
-    }
+
 }
