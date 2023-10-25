d5a524bc49e985c6b1c8d7310c5755080aefe0ab
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-24 19:44:11 -0700

    inside fabtasy Team repo, create method to find User through Id

--- src/main/java/com/example/capstone/repository/FantasyTeamRepository.java
+++ src/main/java/com/example/capstone/repository/FantasyTeamRepository.java
@@ -1,7 +1,12 @@
 package com.example.capstone.repository;
 
+import com.example.capstone.model.FantasyTeam;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public class FantasyTeamRepository {
+public interface FantasyTeamRepository extends JpaRepository<FantasyTeam, Long> {
+    List<FantasyTeam> findByUser_Id(Long userId);
 }
