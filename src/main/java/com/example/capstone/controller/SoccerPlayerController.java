1df15afd83a85c0ec9ee64e176cee2acd2980de3
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-24 18:37:14 -0700

    get test to pass for finding players through name

--- src/main/java/com/example/capstone/controller/SoccerPlayerController.java
+++ src/main/java/com/example/capstone/controller/SoccerPlayerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +31,20 @@ public class SoccerPlayerController {
 
         if (soccerPlayersList.isEmpty()) {
             message.put("message", "Cannot find any soccer players.");
+            return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+        } else {
+            message.put("message", "Success");
+            message.put("data", soccerPlayersList);
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        }
+    }
+    @GetMapping("/name/{name}")
+    public ResponseEntity<?> getSoccerPlayersByName(@PathVariable String name) {
+        List<SoccerPlayer> soccerPlayersList = soccerPlayerService.getSoccerPlayersByName(name);
+        HashMap<String, Object> message = new HashMap<>();
+
+        if (soccerPlayersList.isEmpty()) {
+            message.put("message", "Cannot find any soccer players with the given name.");
             return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
         } else {
             message.put("message", "Success");
