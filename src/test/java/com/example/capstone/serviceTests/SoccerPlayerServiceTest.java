9598023ba143a30ea2eaf39c994f0a8a7a359638
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-24 18:16:31 -0700

    create test to get ALL soccer players, mock service class and crteate a BeforeEach method

--- src/test/java/com/example/capstone/serviceTests/SoccerPlayerServiceTest.java
+++ src/test/java/com/example/capstone/serviceTests/SoccerPlayerServiceTest.java
@@ -1,17 +1,53 @@
 package com.example.capstone.serviceTests;
 
 import com.example.capstone.controller.SoccerPlayerController;
+import com.example.capstone.model.SoccerPlayer;
 import com.example.capstone.service.SoccerPlayerService;
-import com.nimbusds.jose.crypto.impl.AAD;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class SoccerPlayerServiceTest {
-    @Autowired
+    @Mock
     private SoccerPlayerService soccerPlayerService;
     @Autowired
     private SoccerPlayerController soccerPlayerController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        soccerPlayerController = new SoccerPlayerController(soccerPlayerService);
+    }
+
+    @Test
+    void testGetAllSoccerPlayers() {
+        // Mocking the service method
+        when(soccerPlayerService.getAllSoccerPlayers()).thenReturn(List.of(new SoccerPlayer()));
+
+        // Calling the controller method
+        ResponseEntity<?> response = soccerPlayerController.getAllSoccerPlayers();
+
+        // Extracting the soccer players list from the response
+        @SuppressWarnings("unchecked")
+        HashMap<String, Object> responseBody = (HashMap<String, Object>) response.getBody();
+        @SuppressWarnings("unchecked")
+        List<SoccerPlayer> returnedPlayers = (List<SoccerPlayer>) responseBody.get("data");
+
+        // Asserting the size of the returned list
+        assertEquals(1, returnedPlayers.size());
+    }
+
 
 
 }
