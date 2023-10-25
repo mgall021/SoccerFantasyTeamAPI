facf083641f101b3c066ed4847acae389322eea4
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-24 23:23:53 -0700

    create instances of all repos + password encoder

--- src/main/java/com/example/capstone/seed/SeedData.java
+++ src/main/java/com/example/capstone/seed/SeedData.java
@@ -1,7 +1,20 @@
 package com.example.capstone.seed;
 
+import com.example.capstone.repository.FantasyTeamRepository;
+import com.example.capstone.repository.SoccerPlayerRepository;
+import com.example.capstone.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SeedData {
+    private final FantasyTeamRepository fantasyTeamRepository;
+
+    private final SoccerPlayerRepository soccerPlayerRepository;
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+
 }
