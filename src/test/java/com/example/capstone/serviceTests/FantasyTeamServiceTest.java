8913383c21105b665e8454e0773c24291e0ffeb2
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-24 19:50:14 -0700

    create mocks for my servuce test

--- src/test/java/com/example/capstone/serviceTests/FantasyTeamServiceTest.java
+++ src/test/java/com/example/capstone/serviceTests/FantasyTeamServiceTest.java
@@ -1,4 +1,20 @@
 package com.example.capstone.serviceTests;
 
+
+import com.example.capstone.repository.FantasyTeamRepository;
+import com.example.capstone.repository.SoccerPlayerRepository;
+import com.example.capstone.service.FantasyTeamService;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
 public class FantasyTeamServiceTest {
+    @InjectMocks
+    private FantasyTeamService fantasyTeamService;
+
+    @Mock
+    private FantasyTeamRepository fantasyTeamRepository;
+
+    @Mock
+    private SoccerPlayerRepository soccerPlayerRepository;
+
 }
