a628c3af2dc45f169ed0c080e095b8a1b669ace1
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-24 20:05:22 -0700

    create test to create a fantasy Team

--- src/test/java/com/example/capstone/serviceTests/FantasyTeamServiceTest.java
+++ src/test/java/com/example/capstone/serviceTests/FantasyTeamServiceTest.java
@@ -1,13 +1,20 @@
 package com.example.capstone.serviceTests;
 
 
+import com.example.capstone.model.FantasyTeam;
 import com.example.capstone.repository.FantasyTeamRepository;
 import com.example.capstone.repository.SoccerPlayerRepository;
 import com.example.capstone.service.FantasyTeamService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 public class FantasyTeamServiceTest {
     @InjectMocks
@@ -22,6 +29,15 @@ public class FantasyTeamServiceTest {
     @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testCreateFantasyTeam() {
+        FantasyTeam team = new FantasyTeam();
+        when(fantasyTeamRepository.save(any(FantasyTeam.class))).thenReturn(team);
+
+        FantasyTeam result = fantasyTeamService.createFantasyTeam(team);
+        assertEquals(result, team);
     }
 
 
